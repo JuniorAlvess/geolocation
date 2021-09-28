812791c7dd409d1429195276de0a94e1900b109b
Junior Alves <junior@bizsys.com.br>
2021-09-28 18:25:08 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [location, setLocation] = useState();
+  // var watchId;
+  let target = document.getElementById('target');
+
+  const getLocalization = () => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(function (location) {
+        // appendLocation(location, 'fetched');
+        console.log(location);
+        setLocation(location);
+      });
+    } else {
+      target.innerText = 'Geolocation API not supported.';
+    }
+  };
+
+  const showLocation = () => {
+    navigator.geolocation.getCurrentPosition(location => { console.log(location) })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => getLocalization()}>
+        Localização
+      </button>
+
+      <button onClick={() => showLocation()}>
+        show location
+      </button>
+      <div id="target">
+
+      </div>
+      {
+        location !== undefined && (
+          <iframe src={`https://maps.google.com/maps?&z=15&q=${location.coords.latitude}+${location.coords.longitude}&output=embed`} width="100%" height="500"></iframe>
+        )
+      }
+    </>
   );
 }
 
